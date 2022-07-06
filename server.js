5825e1034d809e7fbebe968043768061961d72cf
bohanhou14 <13381498805@163.com>
2022-07-06 18:43:35 +0200

    added the Procfile

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post("/back-to-main", function(req, res){
 
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 });
 
